f41ce44d10124e9f47f4eeca5615a51e93d25f05
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-06-30 13:05:05 +0200

    NavBar component updated

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,6 +6,8 @@
       <RouterLink to="/">Dashboard</RouterLink>
 
       <RouterLink to="/projects"> Projects </RouterLink>
+
+      <RouterLink to="/skills"> Skills </RouterLink>
     </div>
   </nav>
 </template>

7d188e1ee40a10140c42e097ce932dfbb4ddf0cc
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-06-30 13:04:30 +0200

    SkillCard component created

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  skill: Object,
+});
+</script>
+<template>
+  <div class="skill-card">
+    <h2>{{ skill.name }}</h2>
+    <p>{{ skill.category }}</p>
+    <span :class="skill.level">{{ skill.level }}</span>
+  </div>
+</template>
